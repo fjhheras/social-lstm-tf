38f1dfa822ac34380526f9f9c2c85cd460ae84bd
Anirudh Vemula <vvanirudh@gmail.com>
2017-05-19 14:33:49 -0400

    Added validation to train

--- social_lstm/social_train.py
+++ social_lstm/social_train.py
@@ -65,7 +65,7 @@ def main():
     parser.add_argument('--leaveDataset', type=int, default=3,
                         help='The dataset index to be left out in training')
     # Lambda regularization parameter (L2)
-    parser.add_argument('--lambda_param', type=float, default=0.005,
+    parser.add_argument('--lambda_param', type=float, default=0.0005,
                         help='L2 regularization parameter')
     args = parser.parse_args()
     train(args)
@@ -78,9 +78,21 @@ def train(args):
     # datasets = [0]
 
     # Create the SocialDataLoader object
-    data_loader = SocialDataLoader(args.batch_size, args.seq_length, args.maxNumPeds, datasets, forcePreProcess=True)
+    data_loader = SocialDataLoader(args.batch_size, args.seq_length, args.maxNumPeds, datasets, forcePreProcess=True, infer=False)
 
-    with open(os.path.join('save', 'social_config.pkl'), 'wb') as f:
+    # Log directory
+    log_directory = 'log/'
+    log_directory += str(args.leaveDataset) + '/'
+
+    # Logging files
+    log_file_curve = open(os.path.join(log_directory, 'log_curve.txt'), 'w')
+    log_file = open(os.path.join(log_directory, 'val.txt'), 'w')
+
+    # Save directory
+    save_directory = 'save/'
+    save_directory += str(args.leaveDataset) + '/'
+
+    with open(os.path.join(save_directory, 'social_config.pkl'), 'wb') as f:
         pickle.dump(args, f)
 
     # Create a SocialModel object with the arguments
@@ -96,13 +108,18 @@ def train(args):
         saver = tf.train.Saver(tf.all_variables())
 
         # summary_writer = tf.train.SummaryWriter('/tmp/lstm/logs', graph_def=sess.graph_def)
+        print 'Training begin'
+        best_val_loss = 100
+        best_epoch = 0
 
         # For each epoch
         for e in range(args.num_epochs):
             # Assign the learning rate value for this epoch
             sess.run(tf.assign(model.lr, args.learning_rate * (args.decay_rate ** e)))
             # Reset the data pointers in the data_loader
-            data_loader.reset_batch_pointer()
+            data_loader.reset_batch_pointer(valid=False)
+
+            loss_epoch = 0
 
             # For each batch
             for b in range(data_loader.num_batches):
@@ -141,6 +158,7 @@ def train(args):
 
                 end = time.time()
                 loss_batch = loss_batch / data_loader.batch_size
+                loss_epoch += loss_batch
                 print(
                     "{}/{} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}"
                     .format(
@@ -150,10 +168,80 @@ def train(args):
                         loss_batch, end - start))
 
                 # Save the model if the current epoch and batch number match the frequency
+                '''
                 if (e * data_loader.num_batches + b) % args.save_every == 0 and ((e * data_loader.num_batches + b) > 0):
                     checkpoint_path = os.path.join('save', 'social_model.ckpt')
                     saver.save(sess, checkpoint_path, global_step=e * data_loader.num_batches + b)
                     print("model saved to {}".format(checkpoint_path))
+                '''
+            loss_epoch /= dataloader.num_batches
+            log_file_curve.write(str(e)+','+str(loss_epoch)+',')
+            print '*****************'
+
+            # Validation
+            dataloader.reset_batch_pointer(valid=True)
+            loss_epoch = 0
+
+            for b in range(data_loader.num_batches):
+
+                # Get the source, target and dataset data for the next batch
+                # x, y are input and target data which are lists containing numpy arrays of size seq_length x maxNumPeds x 3
+                # d is the list of dataset indices from which each batch is generated (used to differentiate between datasets)
+                x, y, d = data_loader.next_valid_batch()
+
+                # variable to store the loss for this batch
+                loss_batch = 0
+
+                # For each sequence in the batch
+                for batch in range(data_loader.batch_size):
+                    # x_batch, y_batch and d_batch contains the source, target and dataset index data for
+                    # seq_length long consecutive frames in the dataset
+                    # x_batch, y_batch would be numpy arrays of size seq_length x maxNumPeds x 3
+                    # d_batch would be a scalar identifying the dataset from which this sequence is extracted
+                    x_batch, y_batch, d_batch = x[batch], y[batch], d[batch]
+
+                    if d_batch == 0 and datasets[0] == 0:
+                        dataset_data = [640, 480]
+                    else:
+                        dataset_data = [720, 576]
+
+                    grid_batch = getSequenceGridMask(x_batch, dataset_data, args.neighborhood_size, args.grid_size)
+
+                    # Feed the source, target data
+                    feed = {model.input_data: x_batch, model.target_data: y_batch, model.grid_data: grid_batch}
+
+                    train_loss = sess.run(model.cost, feed)
+
+                    loss_batch += train_loss
+
+                loss_batch = loss_batch / data_loader.batch_size
+                loss_epoch += loss_batch
+
+            loss_epoch /= dataloader.valid_num_batches
+
+            # Update best validation loss until now
+            if loss_epoch < best_val_loss:
+                best_val_loss = loss_epoch
+                best_epoch = e
+
+            print('(epoch {}), valid_loss = {:.3f}'.format(e, loss_epoch))
+            print 'Best epoch', best_epoch, 'Best validation loss', best_val_loss
+            log_file_curve.write(str(loss_epoch)+'\n')
+            print '*****************'
+
+            # Save the model after each epoch
+            print 'Saving model'
+            checkpoint_path = os.path.join(save_directory, 'social_model.ckpt')
+            saver.save(sess, checkpoint_path, global_step=e)
+            print("model saved to {}".format(checkpoint_path))
+
+        print 'Best epoch', best_epoch, 'Best validation loss', best_val_loss
+        log_file.write(str(best_epoch)+','+str(best_val_loss))
+
+        # CLose logging files
+        log_file.close()
+        log_file_curve.close()
+
 
 if __name__ == '__main__':
     main()
