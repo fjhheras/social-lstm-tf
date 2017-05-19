b1652c561d766c1f29676fe8fe1717fe28bcbe06
Anirudh Vemula <vvanirudh@gmail.com>
2017-05-19 12:04:45 -0400

    Added validation to utils

--- social_lstm/social_utils.py
+++ social_lstm/social_utils.py
@@ -17,7 +17,7 @@ import random
 # sequence.
 class SocialDataLoader():
 
-    def __init__(self, batch_size=50, seq_length=5, maxNumPeds=40, datasets=[0, 1, 2, 3, 4], forcePreProcess=False):
+    def __init__(self, batch_size=50, seq_length=5, maxNumPeds=40, datasets=[0, 1, 2, 3, 4], forcePreProcess=False, infer=False):
         '''
         Initialiser function for the SocialDataLoader class
         params:
@@ -32,6 +32,7 @@ class SocialDataLoader():
         # self.data_dirs = ['./data/eth/univ', './data/eth/hotel']
 
         self.used_data_dirs = [self.data_dirs[x] for x in datasets]
+        self.infer = infer
 
         # Number of datasets
         self.numDatasets = len(self.data_dirs)
@@ -46,6 +47,9 @@ class SocialDataLoader():
         self.batch_size = batch_size
         self.seq_length = seq_length
 
+        # Validation arguments
+        self.val_fraction = 0.2
+
         # Define the path in which the process data would be stored
         data_file = os.path.join(self.data_dir, "social-trajectories.cpkl")
 
@@ -59,7 +63,8 @@ class SocialDataLoader():
         # Load the processed data from the pickle file
         self.load_preprocessed(data_file)
         # Reset all the data pointers of the dataloader object
-        self.reset_batch_pointer()
+        self.reset_batch_pointer(valid=False)
+        self.reset_batch_pointer(valid=True)
 
     def frame_preprocess(self, data_dirs, data_file):
         '''
@@ -74,6 +79,8 @@ class SocialDataLoader():
         # Each numpy array would be of size (numFrames, maxNumPeds, 3) where each pedestrian's
         # pedId, x, y , in each frame is stored
         all_frame_data = []
+        # Validation frame data
+        valid_frame_data = []
         # frameList_data would be a list of lists corresponding to each dataset
         # Each list would contain the frameIds of all the frames in the dataset
         frameList_data = []
@@ -96,17 +103,23 @@ class SocialDataLoader():
             frameList = np.unique(data[0, :]).tolist()
             # Number of frames
             numFrames = len(frameList)
+            if self.infer:
+                valid_numFrames = 0
+            else:
+                valid_numFrames = int(numFrames * self.val_fraction)
 
             # Add the list of frameIDs to the frameList_data
             frameList_data.append(frameList)
             # Initialize the list of numPeds for the current dataset
             numPeds_data.append([])
             # Initialize the numpy array for the current dataset
-            all_frame_data.append(np.zeros((numFrames, self.maxNumPeds, 3)))
+            all_frame_data.append(np.zeros((numFrames - valid_numFrames, self.maxNumPeds, 3)))
+            # Initialize the numpy array for the current dataset
+            valid_frame_data.append(np.zeros((valid_numFrames, self.maxNumPeds, 3)))
 
             # index to maintain the current frame
             curr_frame = 0
-            for frame in frameList:
+            for ind, frame in enumerate(frameList):
                 # Extract all pedestrians in current frame
                 pedsInFrame = data[:, data[0, :] == frame]
 
@@ -134,8 +147,11 @@ class SocialDataLoader():
                     # Add their pedID, x, y to the row of the numpy array
                     pedsWithPos.append([ped, current_x, current_y])
 
-                # Add the details of all the peds in the current frame to all_frame_data
-                all_frame_data[dataset_index][curr_frame, 0:len(pedsList), :] = np.array(pedsWithPos)
+                if (ind >= valid_numFrames) or (self.infer):
+                    # Add the details of all the peds in the current frame to all_frame_data
+                    all_frame_data[dataset_index][ind - valid_numFrames, 0:len(pedsList), :] = np.array(pedsWithPos)
+                else:
+                    valid_frame_data[dataset_index][ind, 0:len(pedsList), :] = np.array(pedsWithPos)
                 # Increment the frame index
                 curr_frame += 1
             # Increment the dataset index
@@ -143,7 +159,7 @@ class SocialDataLoader():
 
         # Save the tuple (all_frame_data, frameList_data, numPeds_data) in the pickle file
         f = open(data_file, "wb")
-        pickle.dump((all_frame_data, frameList_data, numPeds_data), f, protocol=2)
+        pickle.dump((all_frame_data, frameList_data, numPeds_data, valid_frame_data), f, protocol=2)
         f.close()
 
     def load_preprocessed(self, data_file):
@@ -161,18 +177,24 @@ class SocialDataLoader():
         self.data = self.raw_data[0]
         self.frameList = self.raw_data[1]
         self.numPedsList = self.raw_data[2]
+        self.valid_data = self.raw_data[3]
         counter = 0
+        valid_counter = 0
 
         # For each dataset
         for dataset in range(len(self.data)):
             # get the frame data for the current dataset
             all_frame_data = self.data[dataset]
-            print len(all_frame_data)
+            valid_frame_data = self.valid_data[dataset]
+            print 'Training data from dataset', dataset, ':', len(all_frame_data)
+            print 'Validation data from dataset', dataset, ':', len(valid_frame_data)
             # Increment the counter with the number of sequences in the current dataset
             counter += int(len(all_frame_data) / (self.seq_length+2))
+            valid_counter += int(len(valid_frame_data) / (self.seq_length+2))
 
         # Calculate the number of batches
         self.num_batches = int(counter/self.batch_size)
+        self.valid_num_batches = int(valid_counter/self.batch_size)
         # On an average, we need twice the number of batches to cover the data
         # due to randomization introduced
         self.num_batches = self.num_batches * 2
@@ -237,26 +259,103 @@ class SocialDataLoader():
             else:
                 # Not enough frames left
                 # Increment the dataset pointer and set the frame_pointer to zero
-                self.tick_batch_pointer()
+                self.tick_batch_pointer(valid=False)
 
         return x_batch, y_batch, d
 
-    def tick_batch_pointer(self):
+    def next_valid_batch(self, randomUpdate=True):
+        '''
+        Function to get the next batch of points
+        '''
+        # Source data
+        x_batch = []
+        # Target data
+        y_batch = []
+        # Dataset data
+        d = []
+        # Iteration index
+        i = 0
+        while i < self.batch_size:
+            # Extract the frame data of the current dataset
+            frame_data = self.valid_data[self.valid_dataset_pointer]
+            # Get the frame pointer for the current dataset
+            idx = self.valid_frame_pointer
+            # While there is still seq_length number of frames left in the current dataset
+            if idx + self.seq_length < frame_data.shape[0]:
+                # All the data in this sequence
+                seq_frame_data = frame_data[idx:idx+self.seq_length+1, :]
+                seq_source_frame_data = frame_data[idx:idx+self.seq_length, :]
+                seq_target_frame_data = frame_data[idx+1:idx+self.seq_length+1, :]
+                # Number of unique peds in this sequence of frames
+                pedID_list = np.unique(seq_frame_data[:, :, 0])
+                numUniquePeds = pedID_list.shape[0]
+
+                sourceData = np.zeros((self.seq_length, self.maxNumPeds, 3))
+                targetData = np.zeros((self.seq_length, self.maxNumPeds, 3))
+
+                for seq in range(self.seq_length):
+                    sseq_frame_data = seq_source_frame_data[seq, :]
+                    tseq_frame_data = seq_target_frame_data[seq, :]
+                    for ped in range(numUniquePeds):
+                        pedID = pedID_list[ped]
+
+                        if pedID == 0:
+                            continue
+                        else:
+                            sped = sseq_frame_data[sseq_frame_data[:, 0] == pedID, :]
+                            tped = np.squeeze(tseq_frame_data[tseq_frame_data[:, 0] == pedID, :])
+                            if sped.size != 0:
+                                sourceData[seq, ped, :] = sped
+                            if tped.size != 0:
+                                targetData[seq, ped, :] = tped
+
+                x_batch.append(sourceData)
+                y_batch.append(targetData)
+
+                # Advance the frame pointer to a random point
+                if randomUpdate:
+                    self.valid_frame_pointer += random.randint(1, self.seq_length)
+                else:
+                    self.valid_frame_pointer += self.seq_length
+
+                d.append(self.valid_dataset_pointer)
+                i += 1
+            else:
+                # Not enough frames left
+                # Increment the dataset pointer and set the frame_pointer to zero
+                self.tick_batch_pointer(valid=True)
+
+        return x_batch, y_batch, d
+
+    def tick_batch_pointer(self, valid=False):
         '''
         Advance the dataset pointer
         '''
-        # Go to the next dataset
-        self.dataset_pointer += 1
-        # Set the frame pointer to zero for the current dataset
-        self.frame_pointer = 0
-        # If all datasets are done, then go to the first one again
-        if self.dataset_pointer >= len(self.data):
-            self.dataset_pointer = 0
+        if not valid:
+            # Go to the next dataset
+            self.dataset_pointer += 1
+            # Set the frame pointer to zero for the current dataset
+            self.frame_pointer = 0
+            # If all datasets are done, then go to the first one again
+            if self.dataset_pointer >= len(self.data):
+                self.dataset_pointer = 0
+        else:
+            # Go to the next dataset
+            self.valid_dataset_pointer += 1
+            # Set the frame pointer to zero for the current dataset
+            self.valid_frame_pointer = 0
+            # If all datasets are done, then go to the first one again
+            if self.valid_dataset_pointer >= len(self.valid_data):
+                self.valid_dataset_pointer = 0    
 
-    def reset_batch_pointer(self):
+    def reset_batch_pointer(self, valid=False):
         '''
         Reset all pointers
         '''
-        # Go to the first frame of the first dataset
-        self.dataset_pointer = 0
-        self.frame_pointer = 0
+        if not valid:
+            # Go to the first frame of the first dataset
+            self.dataset_pointer = 0
+            self.frame_pointer = 0
+        else:
+            self.valid_dataset_pointer = 0
+            self.valid_frame_pointer = 0
